30a6df5a15f61927285c54a21a44881cbeafc505
Sami  Sabeeh <samsabeeh6@gmail.com>
2020-10-17 01:32:26 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,7 +2,7 @@ class Transfer
  attr_accessor :sender, :receiver, :amount, :status   
   
   def initialize(sender, receiver, amount)
-    @status = pending
+    @status = 'pending'
     @sender = sender 
     @receiver = receiver 
     @amount = amount
